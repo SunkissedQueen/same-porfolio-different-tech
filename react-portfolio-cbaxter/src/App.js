ecb60322b8bc5f34dd0da8a15fe744209fc365f9
Charlean Baxter <baxtercm@outlook.com>
2023-01-23 14:58:47 -0800

    added react router components

--- react-portfolio-cbaxter/src/App.js
+++ react-portfolio-cbaxter/src/App.js
@@ -1,9 +1,26 @@
 import React from "react"
+import { Routes, Route } from "react-router-dom"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import AboutMe from "./pages/AboutMe"
+import Projects from "./pages/Projects"
+import Contact from "./pages/Contact"
+import Home from "./pages/Home"
+import NotFound from "./pages/NotFound"
 
 const App = () => {
   return (
     <>
       <h3>This is the App Page</h3>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/aboutme" element={<AboutMe />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
     </>
   )
 }
